e2d5263890f79b383c138f27f48ea835f0b489a6
Plinio Sandes dos Santos <pliniogafanha@gmail.com>
2010-12-09 03:06:24 -0200

    Colocando jflex no repositorio e atualizando o jflex para script local

--- faz_e_salva.sh
+++ faz_e_salva.sh
@@ -1,1 +1,1 @@
-jflex L910.jflex && java -jar java-cup-11a.jar L910.cup && mv Lexer.java parser.java sym.java ./src/com/L910/ && echo FEITO !!!
+./jflex-1.4.3/bin/jflex L910.jflex && java -jar java-cup-11a.jar L910.cup && mv Lexer.java parser.java sym.java ./src/com/L910/ && echo FEITO !!!
